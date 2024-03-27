470405adc8f9ea419b13120468dacb09a5602656
Denis Masunaga <denis.masunaga@craftablesoftware.com>
2024-03-27 10:28:18 +0000

    updating eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "@graphql-eslint"],
+  plugins: ["@typescript-eslint"],
   env: {
     browser: false,
     commonjs: true,
